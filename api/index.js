cbdd928c946bd5e0c370788e222b9e4759818714
mayankt28 <mayankt28@gmail.com>
2025-07-11 21:18:13 -0400

    Vercel deployment related changes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const connectDB = require('./config/db');
+const connectDB = require('../src/config/db');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const app = express();
@@ -14,11 +14,12 @@ app.use(cors({
 }));
 
 // Routes
-app.use('/api/v1/auth', require('./routes/auth'));
-app.use('/api/v1/jobs', require('./routes/jobs'));
+app.use('/api/v1/auth', require('../src/routes/auth'));
+app.use('/api/v1/jobs', require('../src/routes/jobs'));
 
 // Connect DB and start server
 connectDB();
-const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 5001;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
 
